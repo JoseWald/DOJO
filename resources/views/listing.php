77668f2c294c50755deaff93b76e2e7b1f4a8bf2
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-10-02 19:51:11 +0300

    creating the wies listing.php (33:09)

---
+++ resources/views/listing.php
@@ -0,0 +1,5 @@
+   <h1><?php echo $heading; ?></h1>
+   <?php foreach ($listings as $listing):  ?>
+           <h1><?php echo $listing["description"]; ?></h1>
+    <?php endforeach; ?>
+  
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('listing',[
+        'heading'=>'latest listing',
+        'listings'=>[
+            [
+                'id'=> 1 ,
+                'title'=>'Listing 1',
+                'description'=>"Arono what to write here"
+            ],
+        ]
+    ]);
 });
 
 Route::get('/hello',function () { 
