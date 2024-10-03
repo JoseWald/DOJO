5ed4bd7feba85a04344894f614c74e2ca8085d73
JoseWald <rajaonarivonyjosewald@gmail.com>
2024-10-03 11:57:01 +0300

    changing the hardcoded listing model into real model by Laravel (59:04)

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,29 +1,11 @@
 <?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return [
-            [
-                'id'=> 1 ,
-                'title'=>'Listing 1',
-                'description'=>"Arono what to write here"
-            ],
-            [
-                'id'=> 2 ,
-                'title'=>'Listing 2',
-                'description'=>"Arono what to write here (2)"
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings=self::all();
-
-        foreach($listings as $list){
-            if($list['id'] == $id){
-                return $list;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
